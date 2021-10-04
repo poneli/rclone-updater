661be08b5344eff3b4681faf58082c12841ccfa6
poneli <33544217+poneli@users.noreply.github.com>
2021-10-04 13:00:35 +0200

    Cleanup semicolons

--- rclone-upgrade-deb.sh
+++ rclone-upgrade-deb.sh
@@ -11,27 +11,27 @@ package="https://downloads.rclone.org/rclone-current-linux-amd64.deb"
 downloadfolder="/change/me/example/directory" # No trailing slash
 #### </VARIABLES>
 if [[ $EUID > 0 ]]; then
-	printf -- "Run with sudo... \n";
+	printf "Run with sudo... \n"
 	exit
 fi
 
 if [[ $latestversion > $currentversion ]]; then
-	printf -- "Downloading to %s... \n" $downloadfolder;
+	printf "Downloading to %s... \n" $downloadfolder
 	wget -q $package -P $downloadfolder
-	printf "Installing update... \n";
+	printf "Installing update... \n"
 	dpkg -i $downloadfolder/*.deb &>/dev/null
 	if [[ $(rclone -V | awk -F'[ ]' '/rclone/ { print substr($2, 2)}') = $latestversion ]]; then
-	  printf -- "rclone upgraded successfully from version %s to %s... \n" $currentversion $latestversion;
-	  printf -- "%(%Y-%m-%d %H:%M:%S)T [SUCCESS] rclone upgraded to %s... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null;
-	  printf -- "Cleaning up %s... \n" $downloadfolder;
+	  printf "rclone upgraded successfully from version %s to %s... \n" $currentversion $latestversion
+	  printf "%(%Y-%m-%d %H:%M:%S)T [SUCCESS] rclone upgraded to %s... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null
+	  printf "Cleaning up %s... \n" $downloadfolder
 	  rm -f $downloadfolder/*.deb
 	else
-	  printf -- "Installation of rclone %s failed... \nTerminated... \n" $latestversion;
-	  printf -- "%(%Y-%m-%d %H:%M:%S)T [ERROR] rclone %s upgrade failed... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null;
-	  printf -- "Cleaning up %s... \n" $downloadfolder;
+	  printf "Installation of rclone %s failed... \nTerminated... \n" $latestversion
+	  printf "%(%Y-%m-%d %H:%M:%S)T [ERROR] rclone %s upgrade failed... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null
+	  printf "Cleaning up %s... \n" $downloadfolder
 	  rm -f $downloadfolder/*.deb
 	fi
 else
-	printf -- "rclone %s is already installed... \nTerminated... \n" $latestversion;
-	printf -- "%(%Y-%m-%d %H:%M:%S)T [INFO] rclone %s is already installed... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null;
+	printf "rclone %s is already installed... \nTerminated... \n" $latestversion
+	printf "%(%Y-%m-%d %H:%M:%S)T [INFO] rclone %s is already installed... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null
 fi
