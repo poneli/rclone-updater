d3a096b2bb3ca1b7268cd240bca904639c8aadb3
poneli <33544217+poneli@users.noreply.github.com>
2021-10-04 09:21:54 +0200

    Replace cmds for latestversion and package variable

--- rclone-upgrade-deb.sh
+++ rclone-upgrade-deb.sh
@@ -5,9 +5,9 @@
 #### Published on: https://github.com/poneli/
 #### =====================================================================
 #### <VARIABLES>
-latestversion=$(curl -s -L https://rclone.org/downloads/ | awk -F'[ <]' '/Rclone Download/ { print substr($5, 2)}')
+latestversion=$(curl -fs https://downloads.rclone.org/version.txt | awk -F'[ ]' '/rclone/ { print substr($2, 2)}')
 currentversion=$(rclone -V | awk -F'[ ]' '/rclone/ { print substr($2, 2)}')
-package=$(echo https://downloads.rclone.org/v$latestversion/rclone-v$latestversion-linux-amd64.deb)
+package=$(echo https://downloads.rclone.org/rclone-current-linux-amd64.deb)
 downloadfolder="/change/me/example/directory" # No trailing slash
 #### </VARIABLES>
 if [[ $EUID > 0 ]]; then
@@ -28,6 +28,8 @@ if [[ $latestversion > $currentversion ]]; then
 	else
 	  printf -- "Installation of rclone %s failed... \nTerminated... \n" $latestversion;
 	  printf -- "%(%Y-%m-%d %H:%M:%S)T [ERROR] rclone %s upgrade failed... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null;
+	  printf -- "Cleaning up %s... \n" $downloadfolder;
+	  rm -f $downloadfolder/*.deb
 	fi
 else
 	printf -- "rclone %s is already installed... \nTerminated... \n" $latestversion;
