ac213f03734c63656978d51fb7ccb3a3683080ea
poneli <elisson.pontus@gmail.com>
2021-10-05 13:41:03 +0200

    Fix log to file when run in cron

--- rclone-updater-rpm.sh
+++ rclone-updater-rpm.sh
@@ -10,32 +10,32 @@ currentversion=$(rclone -V | awk -F'[ ]' '/rclone/ { print substr($2, 2)}')
 package="https://downloads.rclone.org/rclone-current-linux-amd64.rpm"
 downloadfolder="/change/me/example/directory" # No trailing slash
 #### </VARIABLES>
-if [[ $EUID > 0 ]]; then
+if [[ $EUID -gt 0 ]]; then
 	printf "Run with sudo... \n"
 	exit
 fi
 
 if [[ $latestversion > $currentversion ]]; then
-	printf "Downloading to %s... \n" $downloadfolder
+	printf "Downloading to %s... \n" "$downloadfolder"
 	wget -q $package -P $downloadfolder
 	printf "Stopping rclone... \n"
 	systemctl stop rclone.service 2>&1 >/dev/null
 	printf "Installing update... \n"
 	yum localinstall -y $downloadfolder/*.rpm &>/dev/null
 	if [[ $(rclone -V | awk -F'[ ]' '/rclone/ { print substr($2, 2)}') = $latestversion ]]; then
-	  printf "rclone updated successfully from version %s to %s... \n" $currentversion $latestversion
-	  printf "%(%Y-%m-%d %H:%M:%S)T [SUCCESS] rclone updated to %s... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null
+	  printf "rclone updated successfully from version %s to %s... \n" "$currentversion" "$latestversion"
+	  printf -- "%(%Y-%m-%d %H:%M:%S)T [SUCCESS] rclone updated to %s... \n" "$(date +%s)" "$latestversion" | tee -a $downloadfolder/update.log >/dev/null
 	  printf "Starting rclone... \n"
 	  systemctl start rclone 2>&1 >/dev/null
-	  printf "Cleaning up %s... \n" $downloadfolder
+	  printf "Cleaning up %s... \n" "$downloadfolder"
 	  rm -f $downloadfolder/*.rpm
 	else
-	  printf "Installation of rclone %s failed... \nTerminated... \n" $latestversion
-	  printf "%(%Y-%m-%d %H:%M:%S)T [ERROR] rclone %s update failed... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null
-	  printf "Cleaning up %s... \n" $downloadfolder
+	  printf "Installation of rclone %s failed... \nTerminated... \n" "$latestversion"
+	  printf -- "%(%Y-%m-%d %H:%M:%S)T [ERROR] rclone %s update failed... \n" "$(date +%s)" "$latestversion" | tee -a $downloadfolder/update.log >/dev/null
+	  printf "Cleaning up %s... \n" "$downloadfolder"
 	  rm -f $downloadfolder/*.rpm
 	fi
 else
-	printf "rclone %s is already installed... \nTerminated... \n" $latestversion
-	printf "%(%Y-%m-%d %H:%M:%S)T [INFO] rclone %s is already installed... \n" $(date +%s) $latestversion | tee -a $downloadfolder/update.log >/dev/null
+	printf "rclone %s is already installed... \nTerminated... \n" "$latestversion"
+	printf -- "%(%Y-%m-%d %H:%M:%S)T [INFO] rclone %s is already installed... \n" "$(date +%s)" "$latestversion" | tee -a $downloadfolder/update.log >/dev/null
 fi
